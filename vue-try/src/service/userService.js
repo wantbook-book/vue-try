a9151f8bd49ef1dfbfe07526b93e89f35767ee30
方文凯 <2780896955@qq.com>
2022-01-20 05:11:07 -0500

    feature login

--- vue-try/src/service/userService.js
+++ vue-try/src/service/userService.js
@@ -2,8 +2,10 @@ import request from '@/utils/request';
 // 用户注册
 const register = ({ name, telephone, password }) => request.post('auth/register', { name, telephone, password });
 const info = () => request.get('auth/info');
+const login = ({ telephone, password }) => request.post('auth/login', { telephone, password });
 
 export default {
   register,
   info,
+  login,
 };
